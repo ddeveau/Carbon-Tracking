8549deec3c0dc450fd079344d60fb6b88432f3ba
Tahia Emran <tahia.emran@duke.edu>
2017-11-09 12:29:20 -0500

    initial xcode project files

---
+++ DatabasesProject/DatabasesProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DatabasesProject
+//
+//  Created by Tahia Emran on 11/9/17.
+//  Copyright © 2017 Tahia Emran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
