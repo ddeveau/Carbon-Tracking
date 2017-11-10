3ce1690f7a7f143a15e5752ca50e4523c0f77184
Tahia Emran <tahia.emran@duke.edu>
2017-11-09 20:43:26 -0500

    bottom part of profile view

--- DatabasesProject/DatabasesProject/ViewController.swift
+++ DatabasesProject/DatabasesProject/ViewController.swift
@@ -13,9 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // bottom part of the profile view 
+        let profileColl = ProfileCollectionView()
+        self.view.addSubview(profileColl)
+        profileColl.pinTopBottom(v: self.view)
+        profileColl.pinLeft(v: self.view, o: 20)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
